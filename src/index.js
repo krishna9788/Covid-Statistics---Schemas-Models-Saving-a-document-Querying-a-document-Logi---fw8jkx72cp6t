7dcb91f2502fd1f555efab6f8a021328d08bf145
Krishna Kushwaha <120515502+krishna9788@users.noreply.github.com>
2023-04-21 18:15:46 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,119 +1,184 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connection } = require('./connector');
-const { json } = require('express');
 
-app.get("/totalRecovered", async(req, resp)=>{
-    const totalRecovered = await connection.aggregate([
-        {$group:{_id:"total",recovered:{$sum:"$recovered"}}}
-    ])
-        resp.status(200).json({
-            data:{_id: totalRecovered}
-        })
+const { connection } = require("./connector");
 
-    })
-
-// })
-
-app.get("/totalActive", async(req, resp)=>{
-    const totalActive = await connection.aggregate([
-        {$group : {_id: 'total', infected:{$sum: "$infected"}}}
-    ])
-        resp.status(200).json({
-            totalActive
-        })
-    })
-
-app.get("/totalDeath", async(req, resp)=>{
-        const totalDeath = await connection.aggregate([
-            {$group: {_id:"total",death: {$sum: "$death"}}}
-        ])
-        resp.status(200).json({
-            totalDeath
-        })
-    })
-
-app.get("/hotspotStates", async(req, resp)=>{
-    const hotspotStates = await connection.aggregate([
-        {
-            $project:{
-                recovered:1,
-                infected:1,
-                state:1,
-                hotspot:{
-                    $subtract:["$infected","$recovered"]
-                }
-            }
+app.get("/totalRecovered", async (req, res) => {
+  try {
+    const data = await connection.aggregate([
+      {
+        $group: {
+          _id: "total",
+          recovered: {
+            $sum: "$recovered",
+          },
         },
-        {
-            $project:{
-                hotspot:1,
-                infected:1,
-                state:1,
-                rate:{
-                    $divide:["$hotspot","$infected"]
-                }
-            }
-        },
-        {
-            $match:{
-                rate:{$gt:0.1}
-            }
-        },
-        {
-            $project:{
-                _id:0
-                ,state:1,
-                rate:{$round:["$rate",5]}
-            }
-        }
-    ])
-    resp.status(200).json({
-        hotspotStates
-    })
-
-    })
-
-app.get("/healthyStates", async(req, resp)=>{
-    const healthyStates = await connection.aggregate([
-        {
-            $project:{
-                death:1,
-                infected:1,
-                state:1,
-                mortality:{
-                    $divide:["$death","$infected"]
-                }
-            }
-        },
-        {
-            $match:{
-                mortality:{$lt:0.005}
-            }
-        },
-        {
-            $project:{
-                _id:0,
-                state:1,
-                mortality:{
-                    $round:["$mortality",5]
-                }
-            }
-        }
+      },
     ]);
-    resp.status(200).json({healthyStates})
+    res.status(200).json({ data: data[0] });
+  } catch (error) {
+    res.status(500).json({
+      status: "failure",
+      message: error.message,
+    });
+  }
+});
 
-})
+// getting total active covid patients
+app.get("/totalActive", async (req, res) => {
+  try {
+    const data = await connection.aggregate([
+      {
+        $group: {
+          _id: "total",
+          active: {
+            $sum: {
+              $subtract: ["$infected", "$recovered"],
+            },
+          },
+        },
+      },
+    ]);
+    res.status(200).json({ data: data[0] });
+  } catch (error) {
+    res.status(500).json({
+      status: "failure",
+      message: error.message,
+    });
+  }
+});
+// getting total deaths count
+app.get("/totalDeath", async (req, res) => {
+  try {
+    const data = await connection.aggregate([
+      {
+        $group: {
+          _id: "total",
+          death: {
+            $sum: "$death",
+          },
+        },
+      },
+    ]);
+    res.status(200).json({ data: data[0] });
+  } catch (error) {
+    res.status(500).json({
+      status: "failure",
+      message: error.message,
+    });
+  }
+});
 
+//  getting hotspot states
+app.get("/hotspotStates", async (req, res) => {
+  try {
+    const data = await connection.aggregate([
+      {
+        $match: {
+          $expr: {
+            $gt: [
+              {
+                $round: [
+                  {
+                    $divide: [
+                      {
+                        $subtract: ["$infected", "$recovered"],
+                      },
+                      "$infected",
+                    ],
+                  },
+                  5,
+                ],
+              },
+              0.1,
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          state: 1,
+          rate: {
+            $round: [
+              {
+                $divide: [
+                  {
+                    $subtract: ["$infected", "$recovered"],
+                  },
+                  "$infected",
+                ],
+              },
+              5,
+            ],
+          },
+        },
+      },
+    ]);
 
+    res.json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "failure",
+      message: error.message,
+    });
+  }
+});
 
+// gettings healthy states
+app.get("/healthyStates", async (req, res) => {
+  try {
+    const data = await connection.aggregate([
+      {
+        $match: {
+          $expr: {
+            $lt: [
+              {
+                $round: [
+                  {
+                    $divide: ["$death", "$infected"],
+                  },
+                  5,
+                ],
+              },
+              0.005,
+            ],
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          state: 1,
+          mortality: {
+            $round: [
+              {
+                $divide: ["$death", "$infected"],
+              },
+              5,
+            ],
+          },
+        },
+      },
+    ]);
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+    res.json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "failure",
+      message: error.message,
+    });
+  }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
